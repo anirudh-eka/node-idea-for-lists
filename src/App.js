ccc91e2ab253f55888d27e86c62d7c71e18a8b46
Anirudh <adhullip@thoughtworks.com>
2018-02-17 11:19:35 +0530

    quick implementation of idea

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
+      let rootData = {
+          name: 'Living things', nodes: [
+              {
+                  name: 'plants', nodes: [
+                      {
+                          name: 'banyan tree'
+                      },
+                      {
+                          name: 'redwood tree'
+                      }
+                  ]
+              },
+              {
+                  name: 'animals', nodes: [
+                      {
+                          name: 'Tiger', nodes: [
+                              {
+                                  name: 'Bengal Tiger'
+                              },
+                              {
+                                  name: 'Siberian Tiger'
+                              }
+                          ]
+                      }
+                  ]
+              },
+          ]
+      };
+
+      const nodeStyling = {
+          margin: 'auto',
+          width: '400px',
+          paddingTop: '80px',
+          textAlign: 'left'
+
+      }
+
+      return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The Node Idea</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div style={nodeStyling}>
+              <Node data={rootData}></Node>
+          </div>
       </div>
     );
   }
 }
+
+
+function Node(props) {
+    const nodeData = (props.data.nodes ? props.data.nodes : []);
+
+    const nodes = nodeData.map((n, i) => <Node key={i} data={n}/>);
+    return (<Fragment>
+            {props.data.name}
+            <ul>
+                {nodes.map(n => <li>{n}</li>)}
+            </ul>
+    </Fragment>);
+}
+
 
 export default App;
